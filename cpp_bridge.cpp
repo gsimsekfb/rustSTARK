0a506f46639bd31d9e023d5adcf811566ddfb135
Gokhan Simsek <g.simsek@paralect.com>
2021-01-22 17:47:20 +0300

    New updated rescue_verify() function

--- cpp_bridge.cpp
+++ cpp_bridge.cpp
@@ -3,15 +3,25 @@
 // ---------------------------- ethS ---------------------------- //
 
 // --- librescue_prover.a
-extern int rescue_prove(int argc, char** argv);
-extern "C" int rescue_prove_c(int argc, char** argv) {
+extern bool rescue_prove(int argc, char** argv);
+extern "C" bool rescue_prove_c(int argc, char** argv) {
     return rescue_prove(argc, argv);
 }
 
 // --- librescue_verifier.a
-extern int rescue_verify(int argc, char** argv);
-extern "C" int rescue_verify_c(int argc, char** argv) {
-    return rescue_verify(argc, argv);
+extern bool rescue_verify(
+    std::string const& proof,
+    std::string const& public_input,
+    std::string const& parameters,
+    std::string const& annotation_file_name
+);
+extern "C" bool rescue_verify_c(
+    const char* proof,
+    const char* public_input,
+    const char* parameters,
+    const char* annotation_file_name
+) {
+    return rescue_verify(proof, public_input, parameters, annotation_file_name);
 }
 
 // ---------------------------- ethS end ----------------------- //
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod constants;
+
 use ::std::os::raw::c_char;
 use ::std::os::raw::c_int;
 use std::ffi::CString;
@@ -16,9 +18,11 @@ extern "C" {
 
 extern "C" {
     pub fn rescue_verify_c(
-        argc: ::std::os::raw::c_int,
-        argv: *const *const ::std::os::raw::c_char,
-    ) -> ::std::os::raw::c_int;
+        proof:                  *const std::os::raw::c_char,
+        public_input:           *const std::os::raw::c_char,
+        parameters:             *const std::os::raw::c_char,
+        annotation_file_name:   *const std::os::raw::c_char
+    ) -> bool;
 }
 
 pub fn rescue_prove(
@@ -48,39 +52,51 @@ pub fn rescue_prove(
     }    
 }
 
-pub fn rescue_verify(in_file: &str) {
-    let args = [
-        "unused_param", 
-        "--in_file", in_file,
-        "--logtostderr",
-    ];
-    let args_vec: Vec<_> = args.iter().map(|&e| CString::new(e).unwrap())
-                                      .collect();
-    let c_args = args_vec.iter().map(|arg| arg.as_ptr())
-                                .collect::<Vec<*const c_char>>();
+pub fn rescue_verify() -> bool {
     unsafe { 
         println!("\n----------- unsafe C++ call   -----------\n");
-        rescue_verify_c(c_args.len() as c_int, c_args.as_ptr()); 
+
+        // proof
+        let pp = constants::PROOF_HEX.to_owned() + "\0";
+        let proof = pp.as_ptr() as *const _;
+        // public_input
+        let x = r#" {"chain_length":3,"output":["0xdb0a16d9f9cedae","0x244b64cb5a39a2b","0x1f6c22cd3cfdde49","0x4bf27b6fae084cb"]} "#;
+        let xx = x.to_owned() + "\0";
+        let public_input = xx.as_ptr() as *const _;
+        // 
+        // parameters
+        let y = r#" {"stark":{"fri":{"fri_step_list":[1,2,2],"last_layer_degree_bound":1,"n_queries":30,"proof_of_work_bits":20},"log_n_cosets":2}} "#;
+        let yy = y.to_owned() + "\0";
+        let parameters = yy.as_ptr() as *const _;
+        // annotation_file_name
+        let a = "";     // "annotation.txt"
+        let aa = a.to_owned() + "\0";
+        let annotation_file_name = aa.as_ptr() as *const _;
+
+        let result = rescue_verify_c(proof, public_input, parameters, annotation_file_name);
+
         println!("\n----------- end of unsafe C++ -----------\n");
+
+        result
     }    
 }
 
 // --- main
 fn main() {    
-    let first_arg = std::env::args().nth(1).unwrap();
+    // let first_arg = std::env::args().nth(1).unwrap();
 
-    if first_arg == "v" {
-        rescue_verify("/opt/example/proof.json");
-    }
-    else {
-        rescue_prove(
-            "/opt/example/rescue_params.json",
-            "/opt/example/rescue_prover_config.json",      
-            "/opt/example/rescue_public_input.json", 
-            "/opt/example/rescue_private_input.json",
-            "/opt/example/proof.json",        
-        );
-    }
+    // if first_arg == "v" {
+        println!("--- Rust: Proof verified: {:?}", rescue_verify());
+    // }
+    // else {
+    //     rescue_prove(
+    //         "/opt/example/rescue_params.json",
+    //         "/opt/example/rescue_prover_config.json",      
+    //         "/opt/example/rescue_public_input.json", 
+    //         "/opt/example/rescue_private_input.json",
+    //         "/opt/example/proof.json",        
+    //     );
+    // }
     
     // ---- helpers
     // print_type_of(&args);        
